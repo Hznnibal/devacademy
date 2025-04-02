906db84022f393ca14065d67cc8c6b545c99093c
Mustapha Mejri <mustaphamejri80@outlook.fr>
2025-04-02 16:21:30 +0200

    test5

--- lib/authentication/authActions.ts
+++ lib/authentication/authActions.ts
@@ -20,29 +20,20 @@ export async function handleSignIn(formData: FormData, lang: string) {
     });
 
     if (!result || result.error) {
-      console.error("Erreur lors de la connexion:", result?.error); // Ajoute un log pour afficher l'erreur
       throw new Error("Erreur lors de la connexion.");
     }
 
-    // Si la connexion réussit, redirige vers la page dans la langue choisie
     redirect(`/${lang}`);
   } catch (error) {
-    // Gestion des erreurs spécifiques liées à l'authentification
     if (error instanceof AuthError) {
-      console.error("Erreur d'authentification:", error); // Log des erreurs d'authentification
       switch (error.type) {
         case "CredentialsSignin":
           throw new Error("Identifiants invalides.");
         default:
-          throw new Error(
-            "Une erreur est survenue lors de l'authentification."
-          );
+          throw new Error("Une erreur est survenue.");
       }
     }
-
-    // Gestion des autres erreurs
-    console.error("Erreur inconnue:", error);
-    throw new Error("Une erreur est survenue.");
+    throw error;
   }
 }
 
