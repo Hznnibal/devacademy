2f3c7839476593d750f1676749cf2bd4a8b226fe
Mustapha Mejri <mustaphamejri80@outlook.fr>
2025-04-02 15:58:31 +0200

    test3

--- next.config.ts
+++ next.config.ts
@@ -17,23 +17,23 @@ const nextConfig: NextConfig = {
     // removeConsole: process.env.NODE_ENV === "production",
   },
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; img-src 'self' data: files.stripe.com; script-src 'self' 'unsafe-inline';",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         { key: "X-Frame-Options", value: "DENY" },
+  //         { key: "X-Content-Type-Options", value: "nosniff" },
+  //         { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value:
+  //             "default-src 'self'; img-src 'self' data: files.stripe.com; script-src 'self' 'unsafe-inline';",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
