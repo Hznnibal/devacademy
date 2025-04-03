93ef8c696b390ed4616cb97940d4bde63bb3c1bd
Mustapha Mejri <mustaphamejri80@outlook.fr>
2025-04-03 17:10:16 +0200

    test sitemap

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -2,7 +2,7 @@ import { navigationItems as arabicNav } from "@/components/sidenav/SideNavAr/nav
 import { navigationItems as englishNav } from "@/components/sidenav/SideNavEn/nav-links";
 import { NextResponse } from "next/server";
 
-const baseUrl = "https://barmajahacademy.com";
+const baseUrl = "https://barmajah-academy.com/en";
 
 // Fonction pour extraire toutes les URLs des formations
 const getCourseUrls = (navItems: typeof arabicNav) => {
